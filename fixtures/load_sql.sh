e13fe8471f280714b70f2958d11d3207a9aeb76e
dacrujo <dacrujo@cronos.be>
2023-11-09 09:49:35 +0100

    fixtures in sql format and sql load script

---
+++ fixtures/load_sql.sh
@@ -0,0 +1,6 @@
+export PGPASSWORD=postgres
+psql -h localhost -p 5432 -d cv_app_db -U postgres -a -f clientcompanies.sql
+psql -h localhost -p 5432 -d cv_app_db -U postgres -a -f keywords.sql
+psql -h localhost -p 5432 -d cv_app_db -U postgres -a -f jobfunctions.sql
+psql -h localhost -p 5432 -d cv_app_db -U postgres -a -f users.sql cvs.sql
+psql -h localhost -p 5432 -d cv_app_db -U postgres -a -f cvs.sql
